26af4eea7f3b0c14527325f7daf576a461894686
adck872 <fatima.ahmed.3@city.ac.uk>
2024-04-14 14:45:48 +0100

    added GUI
    added gui

---
+++ src/main/java/org/redsox/lancastersfx/LoginController.java
@@ -0,0 +1,18 @@
+package org.redsox.lancastersfx;
+
+public class LoginController {
+}
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private Button LoginButton;
+
+    @FXML
+    void Login(ActionEvent event) {
+
+    }
+
+}

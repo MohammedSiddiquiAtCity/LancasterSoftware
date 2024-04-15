aedd993f8505b217f4184c5c0960f9e7e53a9bd1
JustFirefly <mohammed.siddiqui@city.ac.uk>
2024-04-15 07:50:01 +0100

    Working on InvController

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,8 @@
 module org.redsox.lancastersfx {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
     requires java.sql;
+    requires javafx.base;
+    requires javafx.fxml;
+    requires javafx.controls;
 
     exports org.redsox.lancastersfx.GUI;
     opens org.redsox.lancastersfx.GUI to javafx.fxml;
--- src/main/java/org/redsox/lancastersfx/GUI/InventoryController.java
+++ src/main/java/org/redsox/lancastersfx/GUI/InventoryController.java
@@ -2,11 +2,35 @@ package org.redsox.lancastersfx.GUI;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
 
 import java.io.IOException;
 
 public class InventoryController {
     private Main main;
+
+    @FXML
+    private ListView listOfIngredients;
+
+    @FXML
+    private ComboBox removalReason;
+
+    @FXML
+    void Submit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void decreaseStock(ActionEvent event) {
+
+    }
+
+    @FXML
+    void increaseStock(ActionEvent event) {
+
+    }
+    
     @FXML
     void switchToDishes(ActionEvent event) throws IOException {
         Main.switchToDishes();
